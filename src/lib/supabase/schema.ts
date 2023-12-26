2a4f9f1a9091dfa577eb1c313360055b46793457
alphacoder-mp3 <shindeakshay223@gmail.com>
2023-12-26 10:20:53 +0530

    Generating tables and migrating

--- src/lib/supabase/schema.ts
+++ src/lib/supabase/schema.ts
@@ -1,1 +1,53 @@
-export {};
+import { pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core';
+
+export const workspaces = pgTable('workspaces', {
+  id: uuid('id').defaultRandom().primaryKey().notNull(),
+  createdAt: timestamp('created_at', {
+    withTimezone: true,
+    mode: 'string',
+  }),
+  workspaceOwner: uuid('workspace_owner').notNull(),
+  title: text('title').notNull(),
+  iconId: text('icon_id').notNull(),
+  data: text('data'),
+  inTrash: text('in_trash'),
+  logo: text('logo'),
+  bannerUrl: text('banner_url'),
+});
+
+export const folders = pgTable('folders', {
+  id: uuid('id').defaultRandom().primaryKey().notNull(),
+  createdAt: timestamp('created_at', {
+    withTimezone: true,
+    mode: 'string',
+  }),
+  title: text('title').notNull(),
+  iconId: text('icon_id').notNull(),
+  data: text('data'),
+  inTrash: text('in_trash'),
+  logo: text('logo'),
+  bannerUrl: text('banner_url'),
+  workspaceId: uuid('workspace_id').references(() => workspaces.id, {
+    onDelete: 'cascade',
+  }),
+});
+
+export const files = pgTable('files', {
+  id: uuid('id').defaultRandom().primaryKey().notNull(),
+  createdAt: timestamp('created_at', {
+    withTimezone: true,
+    mode: 'string',
+  }),
+  title: text('title').notNull(),
+  iconId: text('icon_id').notNull(),
+  data: text('data'),
+  inTrash: text('in_trash'),
+  logo: text('logo'),
+  bannerUrl: text('banner_url'),
+  workspaceId: uuid('workspace_id').references(() => workspaces.id, {
+    onDelete: 'cascade',
+  }),
+  folderId: uuid('folder_id').references(() => folders.id, {
+    onDelete: 'cascade',
+  }),
+});
