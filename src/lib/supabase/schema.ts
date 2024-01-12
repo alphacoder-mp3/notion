d25e169b4a291dda32b0f1409fde9a1a06ca7c44
alphacoder-mp3 <shindeakshay223@gmail.com>
2024-01-13 04:49:56 +0530

    Db migrated, schema updated, landing page created, new components have been developed

--- src/lib/supabase/schema.ts
+++ src/lib/supabase/schema.ts
@@ -1,4 +1,14 @@
-import { pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core';
+import {
+  boolean,
+  integer,
+  jsonb,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+} from 'drizzle-orm/pg-core';
+import { prices, subscriptionStatus } from '../../../migrations/schema';
+import { sql } from 'drizzle-orm';
 
 export const workspaces = pgTable('workspaces', {
   id: uuid('id').defaultRandom().primaryKey().notNull(),
@@ -50,4 +60,49 @@ export const files = pgTable('files', {
   folderId: uuid('folder_id').references(() => folders.id, {
     onDelete: 'cascade',
   }),
+});
+
+export const subscriptions = pgTable('subscriptions', {
+  id: text('id').primaryKey().notNull(),
+  userId: uuid('user_id').notNull(),
+  status: subscriptionStatus('status'),
+  metadata: jsonb('metadata'),
+  priceId: text('price_id').references(() => prices.id),
+  quantity: integer('quantity'),
+  cancelAtPeriodEnd: boolean('cancel_at_period_end'),
+  created: timestamp('created', { withTimezone: true, mode: 'string' })
+    .default(sql`now()`)
+    .notNull(),
+  currentPeriodStart: timestamp('current_period_start', {
+    withTimezone: true,
+    mode: 'string',
+  })
+    .default(sql`now()`)
+    .notNull(),
+  currentPeriodEnd: timestamp('current_period_end', {
+    withTimezone: true,
+    mode: 'string',
+  })
+    .default(sql`now()`)
+    .notNull(),
+  endedAt: timestamp('ended_at', {
+    withTimezone: true,
+    mode: 'string',
+  }).default(sql`now()`),
+  cancelAt: timestamp('cancel_at', {
+    withTimezone: true,
+    mode: 'string',
+  }).default(sql`now()`),
+  canceledAt: timestamp('canceled_at', {
+    withTimezone: true,
+    mode: 'string',
+  }).default(sql`now()`),
+  trialStart: timestamp('trial_start', {
+    withTimezone: true,
+    mode: 'string',
+  }).default(sql`now()`),
+  trialEnd: timestamp('trial_end', {
+    withTimezone: true,
+    mode: 'string',
+  }).default(sql`now()`),
 });
