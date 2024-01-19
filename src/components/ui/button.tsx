e16786855fb4713a716f0768529fab99b3f35588
alphacoder-mp3 <shindeakshay223@gmail.com>
2024-01-19 17:51:40 +0530

    Developed authentication

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -7,11 +7,12 @@ import Image from 'next/image';
 import { twMerge } from 'tailwind-merge';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
+        default:
+          ' whitespace-nowrap bg-primary text-lg text-primary-foreground shadow-2xl shadow-indigo-600/50 rounded-lg ',
         destructive:
           'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         outline:
